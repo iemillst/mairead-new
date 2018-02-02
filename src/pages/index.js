336433b268df800b851e0b0bed37e4942b00b9c9
iemillst <30323644+iemillst@users.noreply.github.com>
2018-02-02 13:16:25 +0000

    Updated index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,18 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <div className='gridContainer'>
+    <div
+    style={{
+    display:'grid',
+    gridTemplateColumns: '2fr 1fr 1fr'
+  }}
+    >
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </div>
   </div>
 )
 
