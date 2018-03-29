86e3f1381c851e9e6324c73aa9d74c0c898d022f
iemillst <30323644+iemillst@users.noreply.github.com>
2018-03-29 10:34:28 +0100

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div className='gridContainer'>
     <div>
-      <h1>Hi people</h1>
+      <h1>Alright my peeps!</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
       
